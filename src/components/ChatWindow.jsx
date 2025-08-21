c1301bc5b5018ed7246b82c1a3f83ccdad1d5c34
Shubham singh <shubham.singh@staqo.com>
2025-08-21 15:50:50 +0530

    changes done

--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -4,7 +4,7 @@ export default function ChatWindow({ messages }) {
   return (
     <div className="flex-1 overflow-y-auto p-4 space-y-3">
       {messages.map((msg, i) => (
-        <MessageBubble key={i} role={msg.role} content={msg.content} />
+        <MessageBubble key={i} role={msg.role} content={msg.content}  />
       ))}
     </div>
   );
